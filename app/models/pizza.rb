fc4d03148b528f486f26e4c608d72964fd96b147
Tom Hoenderdos <tomhoenderdos@gmail.com>
2016-01-07 10:51:14 +0100

    Adding has_many relation to Pizza
    Accepting nested attributes so we can save a pizza with ingredients

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,10 @@
 class Pizza < ActiveRecord::Base
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
+
+  validates :name, presence: true
+
+  def to_s
+    name
+  end
 end
